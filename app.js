ad20278d42ddd009a428546f06487307e138709e
Vikttor22 <victoralveslima@gmail.com>
2025-07-11 14:30:41 -0300

    Agora sim tta unificado

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
+
+const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+
+// Configurar pasta de arquivos estáticos antes das rotas
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Configurar view engine e views
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Rota raiz para o front principal
+app.get('/', (req, res) => {
+  res.render('index'); // Renderiza views/index.ejs
+});
+
+// Rotas das vulnerabilidades
 app.use('/', require('./routes/auth'));
 app.use('/sqli', require('./routes/sqli'));
 app.use('/ssrf', require('./routes/ssrf'));
